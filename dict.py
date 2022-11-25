14e4d18dfad809bf3b82c694f20be8a423afb448
Ahnm1 <hansson.alexander@live.se>
2022-11-25 15:03:51 +0100

    all occurrences of C got replaced with 'connection'

--- dict.py
+++ dict.py
@@ -9,29 +9,29 @@ conn = psycopg2.connect(
 )
 
 # Return list from dictionary
-def read_dict(C):
-    cur = C.cursor()
+def read_dict(connection):
+    cur = connection.cursor()
     cur.execute("SELECT id, word, translation FROM dictionary;")
     rows = cur.fetchall()
     cur.close()
     return rows
 
 # Add word to dictionary
-def add_word(C, word, translation):
-    cur = C.cursor()
+def add_word(connection, word, translation):
+    cur = connection.cursor()
     cur.execute(f"INSERT INTO dictionary (word, translation) VALUES ('{word}', '{translation}');")
     cur.close()
 
 # Remove word from dictionary
-def delete_word(C, ID):
-    cur = C.cursor()
+def delete_word(connection, ID):
+    cur = connection.cursor()
     cur.execute(f"DELETE FROM dictionary WHERE id = '{ID}';")
     cur.close()
 
 
 # Save/Commit changes to database
-def save_dict(C):
-    cur = C.cursor()
+def save_dict(connection):
+    cur = connection.cursor()
     cur.execute("COMMIT;")
     cur.close()
 
